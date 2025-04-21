b372cb144fd131b0372d64b23e1c17f028471277
pierrialexandervidmar <pierri@hotmail.com.br>
2025-04-21 09:33:44 -0300

    Initial commit

---
+++ src/main/java/desafio/itau/desafioitau/DesafioitauApplication.java
@@ -0,0 +1,13 @@
+package desafio.itau.desafioitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioitauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioitauApplication.class, args);
+	}
+
+}
